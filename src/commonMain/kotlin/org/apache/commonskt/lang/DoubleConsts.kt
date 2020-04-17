9e7f500a296d5757319a04cad3bbe6285699c636
Alexandre Lombard <alexandre.lombard@utbm.fr>
2020-04-17 17:02:26 +0200

    Update

--- src/commonMain/kotlin/org/apache/commonskt/lang/DoubleConsts.kt
+++ src/commonMain/kotlin/org/apache/commonskt/lang/DoubleConsts.kt
@@ -8,8 +8,20 @@ object DoubleConsts {
     const val EXP_BIAS = 1023
     const val MIN_EXPONENT = -1022
     const val MAX_EXPONENT = 1023
-    const val SIGN_BIT_MASK = 0x8000000000000000UL
-    const val SIGNIF_BIT_MASK = 4503599627370495L
+    /**
+     * Bit mask to isolate the sign bit of a `double`.
+     */
+    val SIGN_BIT_MASK = 0x8000000000000000UL
+
+    /**
+     * Bit mask to isolate the significand field of a `double`.
+     */
+    const val SIGNIF_BIT_MASK = 0x000FFFFFFFFFFFFFL
+
+    /**
+     * The number of logical bits in the significand of a
+     * {@code double} number, including the implicit bit.
+     */
     const val SIGNIFICAND_WIDTH = 53
     const val SIZE = 64
 }
--- src/commonMain/kotlin/org/apache/commonskt/lang/IntegerConsts.kt
+++
@@ -1,8 +0,0 @@
-package org.apache.commonskt.lang
-
-import org.apache.commonskt.PublicApi
-
-@PublicApi
-object IntegerConsts {
-    const val SIZE = 32
-}
